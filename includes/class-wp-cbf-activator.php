9b923fb281f735095ccf817c4ddc94abc8e9a9f7
Channthoeun Ken <channthoeun@live.com>
2017-08-19 15:35:20 +0700

    first commit

---
+++ includes/class-wp-cbf-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.facebook.com/channthoeun.ken
+ * @since      1.0.0
+ *
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ * @author     Channthoeun Ken <channthoeun@gmail.com>
+ */
+class Wp_Cbf_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-cbf-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.facebook.com/channthoeun.ken
+ * @since      1.0.0
+ *
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Cbf
+ * @subpackage Wp_Cbf/includes
+ * @author     Channthoeun Ken <channthoeun@gmail.com>
+ */
+class Wp_Cbf_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
